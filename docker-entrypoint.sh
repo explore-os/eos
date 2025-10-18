28ad208c5f7d4b1ede9394638e8b3b8ac96d9efa
Steve Biedermann <hardliner66@gmail.com>
2025-10-18 13:40:47 +0200

    entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
 
-[ "$#" -eq 0 ] && set -- /usr/local/bin/supervisor
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+    set -- /usr/local/bin/supervisor "$@"
+fi
+
+# else default to run whatever the user wanted like "bash" or "sh"
 exec "$@"
