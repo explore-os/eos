6b6f59b2fd4c8b9832e61edd24316990df6bc919
Steve Biedermann <hardliner66@gmail.com>
2025-10-19 00:40:06 +0000

    fix demo script

--- demos/demo.sh
+++ demos/demo.sh
@@ -6,6 +6,6 @@ pong_actor="/explore/actors/pong"
 actor_script="/explore/examples/ping-pong.rn"
 
 eos spawn --id ping script "$actor_script"
-eos spawn --id pong script "$actor_script
+eos spawn --id pong script "$actor_script"
 
 eos send --sender "$ping_actor" "$pong_actor" '{"state":"ping"}'
